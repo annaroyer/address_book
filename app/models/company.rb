c17dc0c8051456145c5e2443565c77806a93e9ef
Anna Royer <anna.royer@colorado.edu>
2018-07-05 15:28:44 -0600

    add joins table relationship between company and people

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   validates_presence_of :name
+  has_many :company_people
+  has_many :people, through: :company_people
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
   validates_presence_of :first_name, :last_name
-  
+  has_many :company_people
+  has_many :companies, through: :company_people
 end
