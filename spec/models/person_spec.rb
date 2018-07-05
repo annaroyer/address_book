91d39f54911fe787566005cc14ac8027841ebd81
Anna Royer <anna.royer@colorado.edu>
2018-07-05 15:04:06 -0600

    add person model spec

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Person, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+  end
+end
+# Create an address book that can record two types of entities -
+# people (first name, last name)
+# and companies (get company name, company number).
+# the address book can relate people to companies.
+# The address book can record addresses for people and organizations
