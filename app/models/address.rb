9284fa6e45a9c3f6b6b62bb3690eaf5ff72b9a69
Anna Royer <anna.royer@colorado.edu>
2018-07-05 16:11:41 -0600

    add relationships to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ApplicationRecord
   validates_inclusion_of :country,
     in: ['England', 'Wales', 'Scotland', 'Northern Ireland', 'Great Britain', 'United Kingdom', 'Not Specified'],
     on: :create
+  has_many :person_addresses
+  has_many :people, through: :person_addresses
+  has_many :company_addresses
+  has_many :companies, through: :company_addresses
 end
