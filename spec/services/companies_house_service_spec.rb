e5516895d4aeffd61329dc25f5f929e4de895ded
Anna Royer <anna.royer@colorado.edu>
2018-07-06 15:34:02 -0600

    update service spec

--- spec/services/companies_house_service_spec.rb
+++ spec/services/companies_house_service_spec.rb
@@ -5,12 +5,11 @@ describe CompaniesHouseService do
     describe '.get_url' do
       it 'returns the data from a request to the search/companies endpoint' do
         VCR.use_cassette('companies_search') do
-          endpoint = '/search/companies'
           params = { q: 'bankbond' }
-          companies = CompaniesHouseService.get_url(endpoint, params)
+          companies = CompaniesHouseService.search_companies(params)
 
-          expect(companies[:items].count).to eq(3)
-          expect(companies[:items].first[:title]).to eq('BANKBOND LIMITED')
+          expect(companies.count).to eq(3)
+          expect(companies.first.name).to eq('BANKBOND LIMITED')
         end
       end
     end
