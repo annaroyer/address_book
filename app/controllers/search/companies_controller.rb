fc57a290850260f4c145f31399d7dee841c24ca4
Anna Royer <anna.royer@colorado.edu>
2018-07-06 15:33:22 -0600

    refactor

--- app/controllers/search/companies_controller.rb
+++ app/controllers/search/companies_controller.rb
@@ -1,15 +1,10 @@
 class Search::CompaniesController < ApplicationController
   def index
-    host = "https://api.companieshouse.gov.uk"
-    api_key = ENV['companies_house_key']
-    conn = Faraday.new(host) do |request|
-      request.basic_auth(api_key, '')
-      request.adapter Faraday.default_adapter
-    end
-    response = conn.get('/search/companies', {q: 'bankbond'})
-    body = JSON.parse(response.body, symbolize_names: true)
-    @companies = body[:items].map do |company_attrs|
-      Company.new(id: company_attrs[:company_number].to_i, name: company_attrs[:title])
-    end
+    @companies = CompaniesHouseService.search_companies(search_params)
   end
+
+  private
+    def search_params
+      params.permit(:q)
+    end
 end
