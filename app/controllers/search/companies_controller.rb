76800f248b6e8424a1779747d136c9ba76e6b0a8
Anna Royer <anna.royer@colorado.edu>
2018-07-06 14:22:10 -0600

    make call to api and create company objects - will refactor out to services

--- app/controllers/search/companies_controller.rb
+++ app/controllers/search/companies_controller.rb
@@ -1,5 +1,15 @@
 class Search::CompaniesController < ApplicationController
   def index
-    binding.pry
+    host = "https://api.companieshouse.gov.uk"
+    api_key = ENV['companies_house_key']
+    conn = Faraday.new(host) do |request|
+      request.basic_auth(api_key, '')
+      request.adapter Faraday.default_adapter
+    end
+    response = conn.get('/search/companies', {q: 'bankbond'})
+    body = JSON.parse(response.body, symbolize_names: true)
+    @companies = body[:items].map do |company_attrs|
+      Company.new(id: company_attrs[:company_number].to_i, name: company_attrs[:title])
+    end
   end
 end
