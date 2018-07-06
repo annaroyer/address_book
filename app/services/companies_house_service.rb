44442bd0c6c5d7073fa36a2605e2164bb7f41f90
Anna Royer <anna.royer@colorado.edu>
2018-07-06 15:33:41 -0600

    make search companies public method for service - may change later

--- app/services/companies_house_service.rb
+++ app/services/companies_house_service.rb
@@ -1,11 +1,18 @@
 class CompaniesHouseService
 
-  def self.get_url(endpoint, params)
-    response = conn.get(endpoint, params)
-    JSON.parse(response.body, symbolize_names: true)
+  def self.search_companies(params)
+    get_url('/search/companies', params)[:items].map do |attrs|
+      Company.new(id: attrs[:company_number].to_i, name: attrs[:title])
+    end
   end
 
   private
+
+    def self.get_url(endpoint, params={})
+      response = conn.get(endpoint, params)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
     def self.conn
       Faraday.new("https://api.companieshouse.gov.uk") do |request|
         request.basic_auth(ENV['companies_house_key'], '')
