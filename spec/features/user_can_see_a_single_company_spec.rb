5c5ee7d3574b625e4249f5fedfdf0ac0347e65f3
Anna Royer <anna.royer@colorado.edu>
2018-07-07 14:35:58 -0600

    add stubs and fixtures to user sees a single company

--- spec/features/user_can_see_a_single_company_spec.rb
+++ spec/features/user_can_see_a_single_company_spec.rb
@@ -14,9 +14,19 @@ describe 'As a user' do
     end
 
     scenario 'I can click on a company name link and see information about the company' do
+      bankbond_limited = File.open('./spec/fixtures/bankbond_limited.json')
+      bankbond_limited_officers = File.open('./spec/fixtures/bankbond_limited_officers.json')
+      stub_request(:get, 'https://api.companieshouse.gov.uk/company/05275061')
+        .to_return(status: 200, body: bankbond_limited, headers: {})
+      stub_request(:get, 'https://api.companieshouse.gov.uk/company/05275061/officers')
+        .to_return(status: 200, body: bankbond_limited_officers, headers: {})
+
       click_on 'BANKBOND LIMITED'
 
-      expect(current_path).to eq('/companies/5275061')
+      expect(current_path).to eq('/companies/05275061')
+      expect(page).to have_content('BANKBOND LIMITED')
+      expect(page).to have_content('05275061')
+      expect(page).to have_content("Unit 2 Howard Avenue\nBarnstaple, Devon\nEX32 8QA")
     end
   end
 end
