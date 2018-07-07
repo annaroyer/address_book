9aa60c217be7b5c91a93537a96ec5e3b25840e66
Anna Royer <anna.royer@colorado.edu>
2018-07-07 14:36:56 -0600

    simplify api service

--- spec/services/companies_house_service_spec.rb
+++ spec/services/companies_house_service_spec.rb
@@ -6,10 +6,10 @@ describe CompaniesHouseService do
       it 'returns the data from a request to the search/companies endpoint' do
         VCR.use_cassette('companies_search') do
           params = { q: 'bankbond' }
-          companies = CompaniesHouseService.search_companies(params)
+          companies = CompaniesHouseService.get_url('/search/companies', params)
 
-          expect(companies.count).to eq(3)
-          expect(companies.first.name).to eq('BANKBOND LIMITED')
+          expect(companies[:items].count).to eq(3)
+          expect(companies[:items].first[:title]).to eq('BANKBOND LIMITED')
         end
       end
     end
