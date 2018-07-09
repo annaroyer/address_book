469bf6f03cc165caf6e577254f096e0853a7acd1
Anna Royer <anna.royer@colorado.edu>
2018-07-08 20:59:54 -0600

    update person validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ApplicationRecord
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :last_name
   has_many :company_people
   has_many :companies, through: :company_people
   belongs_to :address
+  accepts_nested_attributes_for :address
 end
