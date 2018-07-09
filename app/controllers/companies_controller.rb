ae895fbcab389dc58bec70970b1087000469c712
Anna Royer <anna.royer@colorado.edu>
2018-07-08 19:45:43 -0600

    add create method to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,25 @@
 class CompaniesController < ApplicationController
+  before_action :set_company
+
   def show
-    @company = CompanyPresenter.new(company_params)
+  end
+
+  def create
+    if @company.save
+      flash.now[:notice] = "Company Record added to Address Book"
+    else
+      flash.now[:notice] = "Import Was Unsuccessful"
+    end
+    render :show
   end
 
   private
-    def company_params
+
+    def set_company
+      @company = Company.from_service(companies_house_data)
+    end
+
+    def companies_house_data
       CompaniesHouseService.new('/company').get_data(params[:number])
     end
 end
